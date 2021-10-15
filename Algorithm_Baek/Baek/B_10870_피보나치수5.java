4d954f47c58fa6024aef4d423941f1cad3a7926b
이석규 <likppi10@gmail.com>
2021-10-15 21:58:20 +0900

    B_10872

---
+++ Algorithm_Baek/Baek/B_10870_피보나치수5.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class B_10870_피보나치수5 {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+		
+		System.out.print(pibonachi(num));
+	}
+	static int pibonachi(int num) {
+		if(num == 0) {
+			return 0;
+		}else if(num == 1) {
+			return 1;
+		}else {
+			return pibonachi(num-2)+pibonachi(num-1);
+		}
+	}
+}
