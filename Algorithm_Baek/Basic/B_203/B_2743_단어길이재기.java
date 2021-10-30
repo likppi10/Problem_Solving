d2b620539a6e5e52f8e47e8225ffe6b492709618
이석규 <likppi10@gmail.com>
2021-10-30 22:21:44 +0900

    B_2743

---
+++ Algorithm_Baek/Basic/B_203/B_2743_단어길이재기.java
@@ -0,0 +1,12 @@
+package B_203_자료구조1참고;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_2743_단어길이재기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().length());
+	}
+}
